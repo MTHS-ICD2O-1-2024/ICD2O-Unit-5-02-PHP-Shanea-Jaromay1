07d327460a7d2729e596c0427d53e53393c52bce
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-04-25 03:55:41 +0000

    spelling

--- answer.php
+++ answer.php
@@ -14,7 +14,7 @@
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>TPositive or Negative #, in PHP</title>
+  <title>Positive or Negative #, in PHP</title>
 </head>
 
 <body>
